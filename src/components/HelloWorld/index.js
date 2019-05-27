465c16498a3d08a54b241d73b7c8635bec63699a
MIClavelTe <miclavelte@gmail.com>
2019-05-26 20:50:47 -0700

    Made a HelloWorld component

---
+++ src/components/HelloWorld/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// function HelloWorld(props) {
+//     return <h1>Hello, {props.asdf}</h1>;
+// }
+
+// const HelloWorld = (props) => {
+//     return <h1>Hello, {props.asdf}</h1>;
+// }
+
+const HelloWorld = props => <h1>Hello, {props.name}</h1>
+
+
+export default HelloWorld;
