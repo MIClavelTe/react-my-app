0e3d2aede314ba698040f7b77bbe97e46982fc7a
MIClavelTe <miclavelte@gmail.com>
2019-05-26 22:13:51 -0700

    Using styled components adapting based on props

---
+++ src/components/Button/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Btn = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => props.primary ? "palevioletred" : "white"};
+  color: ${props => props.primary ? "white" : "palevioletred"};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+const Button = (props) => <Btn primary={props.primary}>{props.children}</Btn>
+
+export default Button;
--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import HelloWorld from '../../components/HelloWorld';
 import HelloWorld2 from '../../components/HelloWorld2';
+import Button from '../../components/Button';
 
 const Main = () => (
     <>
         <HelloWorld name="Adriana" color="blue"/>
         <HelloWorld2 />
+        <Button>Button</Button>
+        <Button primary>Primary</Button>
     </>
 )
 
